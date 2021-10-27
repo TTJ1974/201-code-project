40bee1097544ef3ae7acf91483fdbab2d030e36b
TTJ1974 <johntucker1974@live.com>
2021-10-27 16:04:43 -0700

    app.js push

--- .js/app.js
+++ .js/app.js
@@ -1,21 +1,21 @@
-function getUserName(){
-
-  let userName = prompt('Please enter your name!');
-      console.log(userName);
+let questions = [
+  ["What is my first name?", "JOHN", "An outstanding first name, im sure you will agree!"],
+  ["Where was i born?", "MT VERNON", "Heh can you say Tulip Festival!"],
+  ["Did i live in Alaska?", "YES", "Lots of moose up that way, and bears lots of bears!"],
+  ["Gamer or Car buff?", "BOTH", "I like to change it up on occasion!"],
+  ["What is the programing language I really want to learn?", "PYTHON", "Im all about some robot language!"]
+  ];
   
-  if(userName == 'john'){
-      document.write('<h2>Welcome Big Sexy! ' + userName + '</h2>');
-      } else {
-      document.write('<h2>Welcome ' + userName + '</h2>');
+  let correctAnswers = 0;
+  
+  for (let i = 0; i < questions.length; i++) {
+      let answer = prompt(questions[i][0]);
+      if (answer.toUpperCase() == questions[i][1]) {
+          alert("Correct! " + questions[i][2]);
+          correctAnswers++;
       }
-      return userName;
+      else {
+          alert("incorrect, the correct answer was " + questions[i][1]);
+      }
   }
   
-  
-  function beerrules(){
-      let userAnswer = prompt('Do you like beer');
-         // console.log(userAnswer)
-         
-      if(userAnswer.toLowerCase() == 'yes'){
-          let url = "https://i.ibb.co/RBqQVXq/ lightspeed-pilsner-can1-720x.jpg";
-         document.write('<img src="' + url + '">');
