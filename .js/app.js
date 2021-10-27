5390604c929ea2e01055b56cbe09e03d49dcc419
TTJ1974 <johntucker1974@live.com>
2021-10-26 23:22:26 -0700

    added js

---
+++ .js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let userResponse = prompt("Do you like jumping out of planes?");
+
+let userResponse = parseInt(userResponse);
+ if (userResponse ===yes)
+ console.log("Outsdanding Isn't it a rush ?")
+
+
+
