fa52f7efb4afbd177b42eadd6a81c24836e95083
TTJ1974 <johntucker1974@live.com>
2021-10-26 23:36:26 -0700

    updating js

--- .js/app.js
+++ .js/app.js
@@ -1,10 +1,21 @@
-'use strict';
+function getUserName(){
 
-let userResponse = prompt("Do you like jumping out of planes?");
-
-let userResponse = parseInt(userResponse);
- if (userResponse ===yes)
- console.log("Outsdanding Isn't it a rush ?")
-
-
-
+  let userName = prompt('Please enter your name!');
+      console.log(userName);
+  
+  if(userName == 'john'){
+      document.write('<h2>Welcome Big Sexy! ' + userName + '</h2>');
+      } else {
+      document.write('<h2>Welcome ' + userName + '</h2>');
+      }
+      return userName;
+  }
+  
+  
+  function beerrules(){
+      let userAnswer = prompt('Do you like beer');
+         // console.log(userAnswer)
+         
+      if(userAnswer.toLowerCase() == 'yes'){
+          let url = "https://i.ibb.co/RBqQVXq/ lightspeed-pilsner-can1-720x.jpg";
+         document.write('<img src="' + url + '">');
