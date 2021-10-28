bf1145e45b9a9b99095fce439efa898c49d21618
TTJ1974 <johntucker1974@live.com>
2021-10-28 00:02:12 -0700

    lots of css

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 "use strict";
-let questions = [
+//let questions = [
   ["What is my first name?", "JOHN", "An outstanding first name, im sure you will agree!"],
   ["Where was i born?", "MT VERNON", "Heh can you say Tulip Festival!"],
   ["Did i live in Alaska?", "YES", "Lots of moose up that way, and bears lots of bears!"],
   ["Gamer or Car buff?", "BOTH", "I like to change it up on occasion!"],
   ["What is the programing language I really want to learn?", "PYTHON", "Im all about some robot language!"]
-  ];
+  ]; 
   
   let correctAnswers = 0;
   
